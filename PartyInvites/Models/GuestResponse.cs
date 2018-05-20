f598a8f9758770b81204ea61fb70f54ff2dc689d
Sergey Mishin <smishin91@yandex.ru>
2018-05-20 09:41:05 +0300

    PartyInvites MVC

---
+++ PartyInvites/Models/GuestResponse.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PartyInvites.Models
+{
+    public class GuestResponse
+    {
+        //отвечает за храннеие, проверку достоверности и подтверждение ответа на приглашение
+        [Required(ErrorMessage = "Enter name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Enter email")]
+        [EmailAddress(ErrorMessage = "Enter valid email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Enter phone number")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Srecify whether you'll attend")]
+        public bool? WillAttend { get; set; } //true,false,null
+    }
+}
